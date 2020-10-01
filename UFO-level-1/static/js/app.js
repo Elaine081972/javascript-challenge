f40baf24f4a5b9dee9e621eb0d8b02ee6251a50c
Elaine Hamilton <elainehamilton@elaines-mbp.lan>
2020-09-30 18:12:09 -0700

    to update html,app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,7 +4,7 @@ var tableData = data;
 
 
 // get a reference to the table body
-const tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
 
 // loop through the data and use d3 append values
 data.forEach((ufoReport) => {
@@ -17,30 +17,39 @@ data.forEach((ufoReport) => {
 
 // select the button
 // select the form - this doesn't seem to be working????
-let button = d3.select("#filter-btn"),
-    form = d3.select("#filters");
+var button = d3.select("#filter-btn");
+    //form = d3.select("#filters");
 
 // complete the event handler function for the form
 const runEnter = () => {
-
+//button.on("click", function() {
     // prevent the page from refreshing
     d3.event.preventDefault();
+
+    tbody.html("");
     
-    // select the input element and get the raw HTML node
+    // select the input element and get the raw HTML node(these all worked for console log)
     // get the value property of the input element
 
-    let inputElement = d3.select("#datetime"),
-        inputValue = inputElement.property("value");
+        var inputElement = d3.select("#datetime");
+        var inputValue = inputElement.property("value");
 
-    let filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+        var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
    
     // filter is working and returning correct data - need to rerender the table with filtered data
-    console.log(filteredData);
+        console.log(filteredData);
 
-    
-}  
+        filteredData.forEach(function(selections) {
+      
+        var row = tbody.append("tr");
+        Object.entries(selections).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+});        
+};
 
 
 // create event handlers
 button.on("click", runEnter);
-form.on("submit", runEnter);
+//form.on("submit", runEnter);
